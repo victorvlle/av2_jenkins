c09516d95fed49f95289b5ab4fb5cb70bc79b76c
victor <victorvalle100@gmail.com>
2023-10-13 10:16:40 -0400

    Commit

---
+++ src/test:java:com:mycompany:app/TemperatureConverterTest.java
@@ -0,0 +1,27 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void testFahrenheitToCelsius() {
+
+        double fahrenheit1 = 32.0;
+        double expectedCelsius1 = 0.0;
+        double result1 = TemperatureConverter.fahrenheitToCelsius(fahrenheit1);
+        assertEquals(expectedCelsius1, result1, 0.001);
+
+        double fahrenheit2 = 68.0;
+        double expectedCelsius2 = 20.0;
+        double result2 = TemperatureConverter.fahrenheitToCelsius(fahrenheit2);
+        assertEquals(expectedCelsius2, result2, 0.001);
+
+        double fahrenheit3 = -22.0;
+        double expectedCelsius3 = -30.0;
+        double result3 = TemperatureConverter.fahrenheitToCelsius(fahrenheit3);
+        assertEquals(expectedCelsius3, result3, 0.001);
+    }
+}
+
