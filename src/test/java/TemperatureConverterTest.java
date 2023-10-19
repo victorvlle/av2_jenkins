88511ca88e8ed03f47c9b39ebaab3aaf36ff421a
victor <victorvalle100@gmail.com>
2023-10-18 21:55:12 -0400

    Commit

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -6,27 +6,20 @@ import static org.junit.Assert.assertEquals;
 import java.beans.Transient;
 
 public class TemperatureConverterTest {
-/*
-    @Test
-    public void testeSuccess(){
-        assertEquals(-12.222222222222221, TemperatureConverter.fahrenheitToCelsius(10), 0.0);
-        assertEquals(33.8, TemperatureConverter.celsiusToFahrenheit(1), 0.0);
-        assertEquals(41.0, TemperatureConverter.celsiusToFahrenheit(5.0), 0.0);
-    }
- */
+
     @Test
     public void testeFailure(){
         assertEquals(-20, TemperatureConverter.fahrenheitToCelsius(30), 0.0);
-        assertEquals(86.0,TemperatureConverter.celsiusToFahrenheit(31.0),0.0);
-        assertEquals(123.44,TemperatureConverter.celsiusToFahrenheit(65.6), 0.0);
     }
-/* 
+
     @Test
-    public void testeUninstable(){
+    public void testeSuccess(){
         assertEquals(-12.2222, TemperatureConverter.fahrenheitToCelsius(10), 0.0);
-        assertEquals(33.8, TemperatureConverter.celsiusToFahrenheit(1), 0.0);
-        assertEquals(-15.0, TemperatureConverter.fahrenheitToCelsius(5.0), 0.0);
     }
-*/
+
+    @Test
+    public void testeSuccess2(){
+        assertEquals(-12.2222, TemperatureConverter.fahrenheitToCelsius(10), 0.0);
+    }
 }
 
