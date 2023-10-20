baa07a0e3254b35bab573a8dee305f644914a555
victor <victorvalle100@gmail.com>
2023-10-20 11:40:44 -0400

    Commit

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 import java.util.Scanner;
 
-public class TemperatureConverter {
+public class TemperatureConverter{
 
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5.0/9.0;
@@ -12,4 +12,4 @@ public class TemperatureConverter {
         return (celsius * 9.0/5.0) + 32;
     }
 
-}a
+}
